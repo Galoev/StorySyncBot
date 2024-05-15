59e84143f6f1e3f93ba4d089187d64bafda63a61
Galoev <galoyev.ilkin@gmail.com>
2024-05-16 02:46:16 +0300

    fix: async input

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from instaloader import Profile
 from instaloader import LatestStamps
 from telebot import TeleBot
 from dotenv import load_dotenv
+from pathlib import Path
 import asyncio
+import aioconsole
 import os
 
 
@@ -17,16 +19,32 @@ TG_ACCESS_TOKEN=os.getenv("TG_ACCESS_TOKEN")
 
 # Initialize Instaloader
 L = Instaloader(download_video_thumbnails=False)
-L.login(INST_LOGIN, INST_PASSWORD)
+# L.login(INST_LOGIN, INST_PASSWORD)
 stampsDB = LatestStamps("configs/stamps.txt")
 
 # Initialize Telegram Bot
 bot = TeleBot(TG_ACCESS_TOKEN)
 
+def load_session():
+    session_file = Path(f"configs/session-{INST_LOGIN}")
+    print("Loading session...")
+    if not session_file.exists():
+        print("Session file doesn't exist. Try to login..")
+        L.login(INST_LOGIN, INST_PASSWORD)
+        print("Login done")
+        return
+
+    L.load_session_from_file(username=INST_LOGIN, filename=session_file)
+    print("Loading done")
+
+def save_session():
+    print("Saving session...")
+    L.save_session_to_file(filename=f"configs/session-{INST_LOGIN}")
+    print("Saving done")
 
 def download_stories(profile: Profile):
     L.download_stories(userids=[profile], filename_target=profile.userid, latest_stamps=stampsDB, fast_update=True)
-    print("Download: DONE")
+    print("Download done")
 
 async def post_stories(folder_path: str, channel_id: str):
     files = os.listdir(folder_path)
@@ -48,30 +66,30 @@ def delete_all_files_in_folder(folder_path):
     command = f"rm -rf {folder_path}/*"
     os.system(command)
 
-def save_session():
-    print("Saving session...")
-    L.save_session_to_file(filename=f"config/session-{INST_LOGIN}")
-    print("Saving done")
-
 async def cli_interface(stop_event: asyncio.Event):
     while True:
-        command = input("> ")
+        command = await aioconsole.ainput()
         if command == 'close':
+            print("Closing the program...")
             stop_event.set()
+            save_session()
+            print("Closing done")
             break
         else:
             print("Unknown command. Try again.")
-    print("Closing the program...")
 
 async def run(profile: Profile, stop_event: asyncio.Event):
+    count = 0
     while not stop_event.is_set():
+        print(f"iter: {count}")
+        count += 1
         download_stories(profile)
         await post_stories(str(profile.userid), CHANNEL_ID)
         delete_all_files_in_folder(str(profile.userid))
-        await asyncio.sleep(30)
-    save_session()
+        await asyncio.sleep(60)
 
 async def main():
+    load_session()
     profile = Profile.from_username(L.context, TARGET_USERNAME)
     stop_event = asyncio.Event()
     tasks = [run(profile, stop_event), cli_interface(stop_event)]
