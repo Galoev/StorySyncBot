e021137705ca6b547b582a61bfad48b9f76489e0
Galoev <galoyev.ilkin@gmail.com>
2024-07-18 18:56:05 +0300

    add .env.test to .gitignore

--- logger.py
+++ logger.py
@@ -1,7 +1,6 @@
 import logging
-from pathlib import Path
 from datetime import datetime
-import os
+from pathlib import Path
 
 
 def get_logger(name=__name__, level=logging.DEBUG):
@@ -22,15 +21,17 @@ def get_logger(name=__name__, level=logging.DEBUG):
 
     return logger
 
+
 def get_logger_path(name=__name__):
     logs_dir = Path("logs")
     if not logs_dir.exists():
         logs_dir.mkdir()
-    
+
     log_filename = generate_log_filename()
     log_path = logs_dir / Path(log_filename)
-    
+
     return log_path
+
 
 def generate_log_filename(name=__name__):
     current_time = datetime.now()
@@ -41,8 +42,9 @@ def generate_log_filename(name=__name__):
 
 if __name__ == "__main__":
     logger = get_logger()
-    logger.debug('debug message')
-    logger.info('info message')
-    logger.warning('warn message')
-    logger.error('error message')
-    logger.critical('critical message')
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warn message")
+    logger.error("error message")
+    logger.critical("critical message")
+    logger.critical("critical message")
